90c8660213757197a139865cd3920fb5780e4234
Vetrik <vetrik77@gmail.com>
2013-05-15 09:50:59 -0400

    Updated populator.py

--- ProceduralTerrain/populator.py
+++ ProceduralTerrain/populator.py
@@ -21,3 +21,44 @@ class LeafModel():
 		self.np.setTwoSided(True)
 		self.np.setTransparency(TransparencyAttrib.MAlpha)
 		self.np.setDepthWrite(False)
+		
+		maker = CardMaker('leaf')
+		maker.setFrame(-width/2.0, width/2.0, 0, height)
+		#maker.setFrame(0,1,0,1)
+		for i in range(nrplates):
+			if uvlist != None:
+				maker.setUvRange(uvlist[i][0], uvlist[i][1])
+			else:
+				maker.setUvRange(Point2(0,0), Point2(1,0.98))
+			node = self.np.attachNewNode(maker.generate())
+			#node.setTwoSided(True)
+			node.setHpr(i * 180.0 / nrplates,0,0)
+		self.np.flattenStrong()
+		#np.flattenLight()
+		#np.setTwoSided(True)
+		
+		if jitter == -1:
+			self.jitter = height/width/2
+		else:
+			self.jitter = jitter
+			
+copy = NodePath()
+
+tree = LeafModel("Tree 1", 3, 5.0, 5.0, None, 'Bleech.png', None)
+
+def makeTree():
+	np = tree.np.copyTo(copy)
+	#np = self.model.instanceTo(self.grassNP)
+	#np = loader.loadModel('models/grass.egg')
+	#np.reparentTo(self.grassNP)
+	#np.setTwoSided(True)
+	#np.setHpr(Vec3(heading, 0, 0))
+	#np.setPos(pos)
+	#logging.info(np)
+	return np
+	
+sphere = loader.loadModel("models/sphere")
+
+def makeSphere():
+	np = NodePath()
+	sphere.copyTo(np)
