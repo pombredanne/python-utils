8169cc07569547d495ceabc1ff9782f4ebe1dbed
Vetrik <vetrik77@gmail.com>
2013-05-15 06:00:20 -0400

    Complete mapeditor.py

--- ProceduralTerrain/mapeditor.py
+++ ProceduralTerrain/mapeditor.py
@@ -53,3 +53,61 @@ class MapEditor():
 		# Set the position of the ray based on the mouse position
 		if not self.mPickRay.setFromLens(base.camNode, mpos.getX(), mpos.getY()):
 			logging.error("point is not acceptable")
+			
+		# for this small example I will traverse everything, for bigger projects this is probably a bad idea
+		self.mPickerTraverser.traverse(self.terrain)
+		logging.info("Mouse pick ray traversing terrain.")
+		
+		if (self.mCollisionQueue.getNumEntries() > 0):
+			self.mCollisionQueue.sortEntries()
+			entry = self.mCollisionQueue.getEntry(0);
+			pickedObj = entry.getIntoNodePath()
+			#pickedObj = pickedObj.findNetTag('EditableTerrain')
+			if not pickedObj.isEmpty():
+				# here is how you get the surface collision
+				pos = entry.getSurfacePoint(render)
+				#pos.z *= self.terrain.getSz()
+				logging.info(str(pickedObj))
+				logging.info(str(pos))
+				self.cursor.setPos(pos)
+				#handlePickedObject(pickedObj)
+			else:
+				logging.info("picked object is empty")
+		else:
+			logging.info("Nothing collided with mouse pick ray")
+			
+	def setupMouseCollision(self):
+		""" """
+		# Since we are using collision detection to do picking, we set it up
+		# like any other collision detection system with a traverser and a handler
+		self.mPickerTraverser = CollisionTraverser() # make a traverser
+		self.mCollisionQueue = CollisionHandlerQueue()
+		
+		# create a collision solid ray to detect against
+		self.mPickRay = CollisionRay()
+		#self.mPickRay.setOrigin(base.camera.getPos(render))
+		#self.mPickRay.setDirect(render.getRelativeVector(base.camera, Vec3(0,1,0)))
+		
+		# create our collision Node to hold the ray
+		self.mPickNode = CollisionNode('pickRay')
+		self.mPickNode.addSolid(self.mPickRay)
+		
+		# Attach that node to the camera since the ray will need to be position
+		# relative to it, returns a new nodepath
+		# we'll use the default geometry mask
+		# this is inefficient but its for mouse picking only
+		self.mPickNP = base.camera.attachNewNode(self.mPickNode)
+		
+		#well use what panda calls the "from" node.  This is reall a silly convention
+        #but from nodes are nodes that are active, while into nodes are usually passive environments
+        #this isnt a hard rule, but following it usually reduces processing
+
+        #Everything to be picked will use bit 1. This way if we were doing other
+        #collision we could seperate it, we use bitmasks to determine what we check other objects against
+        #if they dont have a bitmask for bit 1 well skip them!
+		self.mPickNode.setFromCollideMask(GeomNode.getDefaultCollideMask())
+		
+		# Register the ray as something that can cause collisions
+		self.mPickerTraverser.addCollider(self.mPickNP, self.mCollisionQueue)
+		# if you want to show collisions for debugging turn this on
+		#self.mPickerTraverser.showCollisions(render)
